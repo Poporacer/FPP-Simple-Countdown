e91c14608668e8774a0bd41615a6b8bedb4b5848
Poporacer <46263877+Poporacer@users.noreply.github.com>
2022-02-01 17:17:59 -0700

    Update fpp_install.sh

--- fpp_install.sh
+++ fpp_install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 pushd $(dirname $(which $0))
 target_PWD=$(readlink -f .)
-/opt/fpp/scripts/update_plugin ${target_PWD##*/}
-/bin/cp RUN-COUNTDOWN-SCRIPT.sh /home/fpp/media/scripts
-. /opt/fpp/scripts/common
+echo ; echo “Please restart fppd for new FPP Commands to be visible.” ; echo
 setSetting restartFlag 1
 popd
